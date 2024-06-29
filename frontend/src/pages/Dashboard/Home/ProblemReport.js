1640ca048fc34de17134dec80667d94594ba6c08
Anshuman Kumar <mahianshuman491@gmail.com>
2024-06-29 14:49:15 +0530

    feedack and report component completed

--- frontend/src/pages/Dashboard/Home/ProblemReport.js
+++ frontend/src/pages/Dashboard/Home/ProblemReport.js
@@ -3,14 +3,15 @@ import homeStyle from "./home.module.css";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import toast from "react-hot-toast";
+import ButtonLoader from "../../../components/ButtonLoader"
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-
 export function ProblemReport() {
   const { instaUserID } = useSelector((state) => state.Instagram);
   const [reportDetails, setReportDetails] = useState({
     subject: "report",
     message: "",
   });
+  const [loading, setLoading] = useState(false)
 
   const handleChange = (e) => {
     setReportDetails({ ...reportDetails, [e.target.name]: e.target.value });
@@ -18,17 +19,29 @@ export function ProblemReport() {
 
   const handleSendReport = (e) => {
     e.preventDefault();
+    setLoading(true)
     axios
       .post(`${BACKEND_URL}send/reportorfeedback/${instaUserID}`, reportDetails)
       .then((response) => {
         if (response.data.success) {
           toast.success(response.data.msg);
+          setReportDetails({
+            subject: "report",
+            message: "",
+          })
+          setLoading(false)
         } else {
           toast.error(response.data.msg);
+          setReportDetails({
+            subject: "report",
+            message: "",
+          })
+          setLoading(false)
         }
       })
       .catch((error) => {
         toast.error(error.message);
+        setLoading(false)
       });
   };
   return (
@@ -74,7 +87,11 @@ export function ProblemReport() {
         >
         </textarea>
 
-        <button className={`${homeStyle.__problemReportBox_button} ${reportDetails.message === "" && "Unactive"}`} type="button" onClick={handleSendReport}> Send</button>
+        <button className={`${homeStyle.__problemReportBox_button} ${reportDetails.message === "" && "Unactive"} ${loading && "Unactive"}`} type="button" onClick={handleSendReport}>
+          {
+            loading ? <ButtonLoader /> : "Send"
+          }
+        </button>
         <span className={`${homeStyle.__problemReportBox_msginfo}`}>
           Your Instagram username and Email address will be automatically included
           in your report.
